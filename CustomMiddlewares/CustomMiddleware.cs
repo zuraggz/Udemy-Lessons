e937e787916887adb7f489d62d034b14b19b4ff6
Zura Gurabanidze <zura.gurabanidze.1@btu.edu.ge>
2026-06-05 17:35:41 +0400

    Custom Middleware

---
+++ CustomMiddlewares/CustomMiddleware.cs
@@ -0,0 +1,12 @@
+﻿namespace Udemy_1.CustomMiddlewares
+{
+    public class CustomMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("CustomMiddleware Before next()");
+            await next(context);
+            await context.Response.WriteAsync("CustomMiddleware After next()");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
+using Udemy_1.CustomMiddlewares;
+
 var builder = WebApplication.CreateBuilder(args);
+// Need to use DI to access custom middleware
+builder.Services.AddTransient<CustomMiddleware>();
 var app = builder.Build();
 
 
@@ -13,6 +17,9 @@ app.Use(async (HttpContext context, RequestDelegate next) =>
     await context.Response.WriteAsync("Max");
     await next(context);
 });
+
+// Auto invoke Method in there
+app.UseMiddleware<CustomMiddleware>();
 
 app.Run(async (HttpContext context) =>
 {
